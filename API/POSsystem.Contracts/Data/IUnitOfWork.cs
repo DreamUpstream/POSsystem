fab363a080fa0452e296d001af0edcb64b73fdd8
Domantas Gruodis <domantas.gruodis@mif.stud.vu.lt>
2022-12-15 20:46:48 +0200

    fixup! Added repositories and added them to the UnitOfWork

--- API/POSsystem.Contracts/Data/IUnitOfWork.cs
+++ API/POSsystem.Contracts/Data/IUnitOfWork.cs
@@ -4,7 +4,17 @@ namespace POSsystem.Contracts.Data
 {
     public interface IUnitOfWork
     {
+        IBranchRepository Branches { get; }
+        ICompanyRepository Companies { get; }
+        ICustomerRepository Customers { get; }
+        IDiscountRepository Discounts { get; }
+        IEmployeeRepository Employees { get; }
+        IItemCategoryRepository ItemCategories { get; }
         IItemRepository Items { get; }
+        IOrderRepository Orders { get; }
+        IPurchasableItemRepository PurchasableItems { get; }
+        IServiceRepository Services { get; }
+        IServiceReservationRepository ServiceReservations { get; }
         IUserRepository Users { get; }
         Task CommitAsync();
     }
